10dc94ff2064ec3ddcfe9fc6444c77fd497384ec
Paul <Paul@192.168.0.3>
2017-03-08 21:32:38 +0100

    alle zahlen werden berechnet

--- Informatik Oberstufe/src/GoldbachscheVermutung.java
+++ Informatik Oberstufe/src/GoldbachscheVermutung.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class GoldbachscheVermutung {
 
 	public static ArrayList<Integer> primZahlen = new ArrayList<Integer>(); 
-	public static int eingabe1 =Integer.parseInt(JOptionPane.showInputDialog("Deine Eingabe"))+4;
+	public static int eingabe1 = /*Integer.parseInt(JOptionPane.showInputDialog("Deine Eingabe"))*/ 15000+4;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		long Time = System.nanoTime();
@@ -26,31 +26,38 @@ public class GoldbachscheVermutung {
 				primZahlen.add(i);
 			}
 		}
-		//System.out.println(primZahlen);
-		//System.out.println(primZahlen.size());
+		//primZahlen.add(primZahlen.size()-1);
 	}
 	public static void  berechnung()
 	{
 		int eingabe2 = eingabe1-4;
-		int i = 0;
 		boolean gefunden = false;
-		for(int k = 0; k < eingabe2; i=i+2)
+		for(int k = 0; k <= eingabe2;)
 		{
-			while(primZahlen.get(i) < eingabe2 && !gefunden)
+			int i = 0;
+			while(primZahlen.get(i) < k && !gefunden)
 			{
 				int j = 0;
-				while(primZahlen.get(j) < primZahlen.size() && primZahlen.get(i)+primZahlen.get(j) <= eingabe2)
+				while(primZahlen.get(j) < primZahlen.size() && primZahlen.get(i)+primZahlen.get(j) <= k)
 				{
-					if(primZahlen.get(j)+primZahlen.get(i)==eingabe2)
+					if(primZahlen.get(i)+primZahlen.get(j)==k)
 					{
-						System.out.println(primZahlen.get(i)); 
+						/*System.out.println(primZahlen.get(i)); 
 						System.out.println(primZahlen.get(j));
-						gefunden = true;
-					}
+						System.out.println(k);*/					
+						if(primZahlen.get(j)+primZahlen.get(i)==eingabe2)
+						{
+							/*System.out.println(primZahlen.get(i)); 
+							System.out.println(primZahlen.get(j));
+							System.out.println("Ende bei " + eingabe2);*/
+							gefunden = true;
+						}
+					}	
 					j++;
 				}
 				i++;
 			}
+			k=k+2;
 		}
 	}
 }
