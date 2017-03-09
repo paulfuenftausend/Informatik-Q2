e00b520a9f7f2f262793ac18491423a1df4a5570
Paul <Paul@192.168.0.3>
2017-03-09 16:03:33 +0100

    angefangen von hinten zu zählen

--- Informatik Oberstufe/src/GoldbachscheVermutung.java
+++ Informatik Oberstufe/src/GoldbachscheVermutung.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class GoldbachscheVermutung {
 
 	public static ArrayList<Integer> primZahlen = new ArrayList<Integer>(); 
-	public static int eingabe1 = /*Integer.parseInt(JOptionPane.showInputDialog("Deine Eingabe"))*/ 15000+4;
+	public static int eingabe1 = /*Integer.parseInt(JOptionPane.showInputDialog("Deine Eingabe"))*/ 100+4;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		long Time = System.nanoTime();
@@ -32,30 +32,38 @@ public class GoldbachscheVermutung {
 	{
 		int eingabe2 = eingabe1-4;
 		boolean gefunden = false;
-		for(int k = 0; k <= eingabe2;)
+		for(int k = 4; k <= eingabe2 ;)
 		{
-			int i = 0;
-			while(primZahlen.get(i) < k && !gefunden)
+			int i = primZahlen.size()-1;
+			
+			while(primZahlen.get(i-1) > k && !gefunden)
 			{
 				int j = 0;
-				while(primZahlen.get(j) < primZahlen.size() && primZahlen.get(i)+primZahlen.get(j) <= k)
+				boolean gefunden2 = false;
+				System.out.println("signal");
+				while(!gefunden2 && j < primZahlen.size()-1)
 				{
-					if(primZahlen.get(i)+primZahlen.get(j)==k)
+					System.out.println(primZahlen.get(i)+" i");
+					System.out.println(primZahlen.get(j)+" j");
+					System.out.println(k+" k");
+					/*System.out.println(primZahlen.size()+" size");*/
+					if(primZahlen.get(i) + primZahlen.get(j)==k)
 					{
-						/*System.out.println(primZahlen.get(i)); 
+						System.out.println(primZahlen.get(i)); 
 						System.out.println(primZahlen.get(j));
-						System.out.println(k);*/					
+						System.out.println(k);
+						gefunden2=true;
 						if(primZahlen.get(j)+primZahlen.get(i)==eingabe2)
 						{
-							/*System.out.println(primZahlen.get(i)); 
+							System.out.println(primZahlen.get(i)); 
 							System.out.println(primZahlen.get(j));
-							System.out.println("Ende bei " + eingabe2);*/
+							System.out.println("Ende bei " + eingabe2);
 							gefunden = true;
 						}
 					}	
 					j++;
 				}
-				i++;
+				i--;
 			}
 			k=k+2;
 		}
